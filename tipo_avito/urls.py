8eede6a64239a5c695ac6a3df5f470ab8c44f400
Igotres <irapb@inbox.ru>
2024-01-16 12:09:41 +0300

    tests.http

--- tipo_avito/urls.py
+++ tipo_avito/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from ads import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.root)
+    path('', include("ads.urls"))
 ]
